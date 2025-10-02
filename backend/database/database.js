8f6c18e63a5ff1d2103747609795b69f1f84d474
Juan Felipe J.Z.. Zea Sanchez <juan.zea@db-system.corp>
2025-10-02 11:40:35 -0500

    Set up database connection

---
+++ backend/database/database.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DATABASE_NAME, 
+    process.env.DATABASE_USERNAME, 
+    process.env.DATABASE_PASSWORD, 
+    {
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        dialect:'mysql',
+        dialectOptions: {
+            connectTimeout: (process.env.DATABASE_TIMEOUT ?? 60000),
+        }
+    });
+
+try 
+{
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} 
+catch (error) 
+{
+    console.error('Unable to connect to the database:', error);
+}
+
+module.exports = sequelize;
