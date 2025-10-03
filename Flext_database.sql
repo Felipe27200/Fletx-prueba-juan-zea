687ed94d8f0fc21fe7c32306b5ecf656f8572682
Felipe Zea <48108543+Felipe27200@users.noreply.github.com>
2025-10-03 16:28:26 -0500

    add DB sql

---
+++ Flext_database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE products (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  price INT NOT NULL,
+  quantity INT NOT NULL
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  refresh_token VARCHAR(255) NULL
+);
+
