c26c94a8b9ecc12b2eb6c729762f55a5bc6676bc
Felipe Zea <48108543+Felipe27200@users.noreply.github.com>
2025-10-03 13:59:27 -0500

    Architecture products completed

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { LoginComponent } from './login/login.component';
 import { SignUp } from './sign-up/sign-up';
 
 export const routes: Routes = [
+    {
+        path: 'products',
+        loadChildren: () => import('./routes/product.routes').then(m => m.PRODUCT_ROUTES)
+    },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignUp },
     { path: "", redirectTo: '/login', pathMatch: 'full' },
